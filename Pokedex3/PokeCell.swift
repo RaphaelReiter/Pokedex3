ccccdec933a106dd1a08f385af152072010bd6fb
RaphaelReiter <rdso.reiter@gmail.com>
2017-05-26 17:36:02 +0200

    The whole app

---
+++ Pokedex3/PokeCell.swift
@@ -0,0 +1,34 @@
+//
+//  PokeCell.swift
+//  Pokedex3
+//
+//  Created by Raphael Reiter on 24/05/2017.
+//  Copyright © 2017 Raphael Reiter. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+    
+    
+}
---
+++ Pokedex3/constants.swift
@@ -0,0 +1,15 @@
+//
+//  constants.swift
+//  Pokedex3
+//
+//  Created by Raphael Reiter on 26/05/2017.
+//  Copyright © 2017 Raphael Reiter. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co/"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+
+typealias DownloadComplete = () -> ()
